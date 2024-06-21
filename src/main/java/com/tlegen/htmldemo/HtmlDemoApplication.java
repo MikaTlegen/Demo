0f015b6b127f18630d356d88926dbe30d08dd395
Ermukhambetov Tlegen <Tlegen13@mail.ru>
2024-06-21 13:52:46 +0500

    first commit

---
+++ src/main/java/com/tlegen/htmldemo/HtmlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tlegen.htmldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tlegen/htmldemo/model/HelloModel.java
@@ -0,0 +1,4 @@
+package com.tlegen.htmldemo.model;
+
+public interface HelloModel {
+}
