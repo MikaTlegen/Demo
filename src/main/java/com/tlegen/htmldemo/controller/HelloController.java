6807a94a41efd56d66e46ef4fbd646ea16666f3e
Tlegen <tlegen13@mail.ru>
2024-06-22 15:45:00 +0500

    Update HelloController.java

--- src/main/java/com/tlegen/htmldemo/controller/HelloController.java
+++ src/main/java/com/tlegen/htmldemo/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String seiHello() {
-        return "Hello World";
+        return "Hello ";
     }
 }
